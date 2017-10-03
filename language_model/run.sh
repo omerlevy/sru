e41a26a1310f3bc2a450140c6fdc3c49815758d5
omerlevy <omerlevy@gmail.com>
2017-10-02 22:21:46 -0700

    cont.

--- language_model/run.sh
+++ language_model/run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=${1} python train_lm.py --train train.txt --dev valid.txt --test test.txt --depth $2 --d 500 --omer --content $3 --gates $4 > logs/L${2}.${3}.${4}
+CUDA_VISIBLE_DEVICES=${1} python -u train_lm.py --train train.txt --dev valid.txt --test test.txt --depth $2 --d 500 --omer --content $3 --gates $4 > logs/L${2}.${3}.${4}
